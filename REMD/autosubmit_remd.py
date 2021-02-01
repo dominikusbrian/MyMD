5b1784e84330a3d63f2df89ecab036933fbad132
domi <dominikusbrian@live.com>
2021-02-01 17:18:10 +0800

    updating REMD

--- REMD/autosubmit_remd.py
+++ REMD/autosubmit_remd.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+import os 
+os.system("echo Hello from the other side!")
+
+setup_equil()
+
 def run_energy_minimization():
     pass
 
 def setup_equil():
-    pass
+    os.system("./setup_equilibrate_input.x")
+    os.system("echo executing setup_equilibrate_input.x")
 
 def run_heat_and_equil():
     pass
