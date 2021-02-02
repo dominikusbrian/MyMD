c186d254a3743a57f729d2520ad1f73fac4c2171
dominikusbrian <dominikusbrian@live.com>
2021-02-02 16:30:20 +0800

    template update

---
+++ MDConsole/analysis.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+# For analysis functions
+
+
+def calc_acceptancerate():
+	'''
+	Function to calculate for Acceptance rate
+	'''
---
+++ MDConsole/console.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+# For simulation control functions
+
