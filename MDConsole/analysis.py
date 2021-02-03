ed6fa6aff63a972ed21a07f0824d0c20f3c21634
dominikusbrian <dominikusbrian@live.com>
2021-02-03 19:18:10 +0800

    updating format

--- MDConsole/analysis.py
+++ MDConsole/analysis.py
@@ -9,8 +9,6 @@ import ArtisanKit as ak
 
 def calc_acceptancerate(filename, whichtemp,print_result):
     '''
-	Function to calculate for Acceptance rate from AMBER rem.log file
-
     filename = remd log file
     whichtemp = which temperature replica , take scalar 1.0, 2.0 , and so on
     print_result = True / False
@@ -31,10 +29,22 @@ def calc_acceptancerate(filename, whichtemp,print_result):
     log= np.loadtxt(filename)
     df = pd.DataFrame(log)
     temp= df.loc[df[0] == whichtemp]
-    #Column 6 is the from typical amber format for rem.log file 
-    ave_acceptance = np.mean(temp[6]) * 100
+    #rem.log column format:
+    #Column 2 = current temperature
+    #Column 3 = potential energy
+    #Column 6 = success rate
 
+    ave_acceptance = np.mean(temp[6]) * 100
+    temp_traj = temp[2]
+    Epot = temp[3]
+    
+    # If zero is ignored
+    
+    #temp[temp == 0] = np.nan
+    #print(temp[6])
+    
+    #ave_acceptance = np.nanmean(temp.loc[6]) * 100
     if(print_result == True):
         ak.iostream.print_dec("My acceptance rate = ",ave_acceptance, "%", 2)
     else:
-        return ave_acceptance
+        return ave_acceptance , temp_traj, Epot
