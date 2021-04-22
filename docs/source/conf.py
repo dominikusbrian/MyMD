3a186adc73fa436f5b1c5e4a9060c2534b95b27e
Domi <dominikusbrian@live.com>
2021-04-22 19:47:03 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,16 +49,16 @@ exclude_patterns = []
 # a list of builtin themes.
 
 
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 
-extensions = [
-    ...
-    "sphinx_rtd_theme",
-]
+# extensions = [
+#     ...
+#     "sphinx_rtd_theme",
+# ]
 
-html_theme = "sphinx_rtd_theme"
-#
-# html_theme = 'alabaster'
+# html_theme = "sphinx_rtd_theme"
+# #
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
